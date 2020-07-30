dd92bea46311c0bcf36ad6b4f4ddb0472d6cdb8c
Carlo Echeverría <carlo.echeverria@cepal.org>
2020-07-29 20:46:55 -0400

    Primer avance para carrusel para las vitrinas

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "../styles/layout.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Layout = ({ children }) => {
 
--- src/templates/showcase.js
+++ src/templates/showcase.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import Layout  from "../components/layout";
+import { Carousel } from 'react-bootstrap';
+import Img from "gatsby-image"
 
 import { graphql } from "gatsby"
 
+const Showcase = (props) => {
+    return (
+        <Layout>
+            <h2>{props.data.nodeShowcase.title}</h2>
+            <Carousel>
+                { props.data.nodeShowcase.relationships.field_showcase_image.map(field => {
+                    return (
+                        <Carousel.Item>
+                            <Img src={field.localFile.url} fluid/>
+                        </Carousel.Item>
+                    )
+                })}
+            </Carousel>
+        </Layout>
+    )
+}
+
+export default Showcase
 export const query = graphql`
     query($drupal_internal__nid: Int) {
         nodeShowcase(drupal_internal__nid: { eq: $drupal_internal__nid }) {
             drupal_internal__nid
             title
+            relationships {
+                field_showcase_image {
+                    localFile {
+                        url
+                    }
+                }
+            }
         }
     }`
-
-const Object = (props) => {
-    return (
-        <Layout>
-            <h1>{props.data.nodeShowcase.title}</h1>
-        </Layout>
-    )
-}
-
-export default Object
